0056c84eafc94185bc3cdead733a96ba5d58ba8c
krakvinme <krakvinme@gmail.com>
2025-02-11 19:34:36 +0300

    init

---
+++ js/constants/root.js
@@ -0,0 +1,5 @@
+const ROOT_PRODUCTS = document.getElementById('products');
+const ROOT_COUNTER = document.getElementById('cart_counter');
+const ROOT_SHOPPING = document.getElementById('shopping');
+const ROOT_SPINNER = document.getElementById('spinner');
+const ROOT_ERROR = document.getElementById('error');
